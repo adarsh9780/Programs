02f5387a4b660a8633a3b8970875210de3eb2068
adarsh9780 <adarshmaurya7@gmail.com>
2017-07-11 21:54:08 +0530

    Remove irrelevant files

--- c++/t.cpp
+++ c++/t.cpp
@@ -1,45 +1,56 @@
 /*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+void print(int max1, int max2, vector<int> array)
+{
+    cout << max1 << " " << max2 << " ";
+    vector<int> :: iterator it = array.begin();
+    while(it != array.end())
+    {
+        if(*it != max1 && *it != max2)
+        {
+            cout << *it << " ";
+        }
+        it++;
+    }
+}
 
 int main()
 {
     //Write code here
-    int len_of_array;
-    int item=0, count=0;
-    int num_to_match;
-    vector<int> array;
-    
-    cout << "Length of array : ";
-    cin >> len_of_array;
-    while(len_of_array!=0)
+    int array_len=0, item=0;
+    vector<int> array, copy;
+    cin >> array_len;
+    cout << "Array length is : " << array_len << endl;
+    while(array_len != 0)
     {
         cin >> item;
         array.push_back(item);
-        len_of_array--;
+        array_len--;
+        cout << "array_len is : " << array_len << endl;
     }
-    cout << "Length of array now : " << len_of_array << endl; 
+
+    copy = array;
     
-    cout << "Before Matching : " << count << endl;
-    
+    sort(array.begin(), array.end());
+    int max1 = *(array.end()-1);
+    int max2 = *(array.end()-2);
+
     vector<int> :: iterator it = array.begin();
-    
-    cout << "Value to be Matched : ";
-    cin >> num_to_match;
-    
-    cout << "Print\n";
     while(it != array.end())
     {
         cout << *it << endl;
-        if(*it > num_to_match)
-        {
-            cout << *it << "==" << num_to_match << endl;
-            count = count + 1;
-        }
         it++;
-        // count = count-1;
     }
-    cout << "count is :" << count << endl;
+
+    print(max1, max2, copy);
+
+    cout << "Max1 : " << max1 << endl;
+    cout << "Max2 : " << max2 << endl;
+    // print(max1, max2, array);
+
     return 0;
 }
